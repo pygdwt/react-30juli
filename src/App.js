c4d1d428ad2dc2fcebf0f8d7a6fcfbf199a3bd27
Yogo <68355889+pygdwt@users.noreply.github.com>
2020-07-30 21:57:05 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>Sabar aja</h2>
+      <button onClick={increment}>Clicked {count} times</button>
+    </div>
+  );
+};
+
+/*class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>hanjink</h2>
+        <button onClick={this.increment}>
+          Clicked {this.state.count} times{" "}
+        </button>
+      </div>
+    );
+  }
+}*/
+
+export default App;
